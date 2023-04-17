d238a86e334d51622fea8fcf91d9c2043a6c58a5
xaruFushigi <xaru.fushigi@gmail.com>
2023-04-17 19:21:32 +0900

    remember-me-passport
    useEffect has bug here, need to fix

--- signinform-front-end/src/components/SignInForm/SignInForm.js
+++ signinform-front-end/src/components/SignInForm/SignInForm.js
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const SignInForm = (props) => {
     
     const [signInEmailInput, setSignInEmailInput]       = useState("");
     const [signInPasswordInput, setSignInPasswordInput] = useState("");
-    const [getUserName, setGetUserName] = useState(null);
+    const [rememberMeCheckbox, setRememberMeCheckbox]   = useState(null);
+    const [getUserName, setGetUserName] = useState(false);
     //get email input
     const onEmailChange = (event) => {setSignInEmailInput(event.target.value)};
     //get password input
     const onPasswordChange = (event) => {setSignInPasswordInput(event.target.value)};
+    //
+    const onCheckboxChange = (event) => {setRememberMeCheckbox(event.target.value)};
     //Sign In button press function
     const onSignInButtonPress = (event) => {
         //to prevent refresh of the webpage
@@ -35,6 +38,9 @@ const SignInForm = (props) => {
             if(data && data.user) {
                 props.loadUsersDataFromDatabase(data);
                 props.SignInPageConditionChangeFunction();
+                if (rememberMeCheckbox) {
+                    localStorage.setItem('user', JSON.stringify(data.user));
+                }
             };
         })
         .catch((error) => {
@@ -42,7 +48,17 @@ const SignInForm = (props) => {
             if(error.response) error.response.text().then((text)=> console.log(text))
         })
     }; 
-   
+
+    useEffect(() => {  //Without the useEffect hook, the code would try to set the value of rememberMeCheckBox 
+                       //->to true immediately after rendering the component, but since the document.cookie property is not available during server rendering and may not be immediately available after the component mounts, the value of rememberMeCheckBox may not be set correctly. 
+        const user = JSON.parse(localStorage.getItem('user')); //the user variable is being used to check if the user has already logged in before and if so, to automatically log them back in by setting the appropriate session data.
+        if (user) {
+            props.loadUsersDataFromDatabase({user});    
+         //   props.SignInPageConditionChangeFunction();
+        }
+    }, [props]); // Using the useEffect hook with an empty dependency array ([]) ensures 
+                 //->that the code to set the state variable is only executed once after 
+                 //->the component mounts, which ensures that the value of rememberMeCheckBox is set correctly.
     return(
         <div>
             <main className="pa4 black-80">
@@ -94,7 +110,7 @@ const SignInForm = (props) => {
                         </div>
                             {/* Remember me checkbox */}
                         <label className="pa0 ma0 lh-copy f6 pointer">
-                            <input type="checkbox"  />
+                            <input type="checkbox" name='remember' onChange={onCheckboxChange} />
                             Remember me
                         </label>
 
