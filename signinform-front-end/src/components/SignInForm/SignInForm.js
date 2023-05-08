46f33c093b87d10af62c70cdd03948c27a8e4089
xaruFushigi <xaru.fushigi@gmail.com>
2023-05-08 22:11:31 +0900

    front-end removal of unnecessary

--- signinform-front-end/src/components/SignInForm/SignInForm.js
+++ signinform-front-end/src/components/SignInForm/SignInForm.js
@@ -10,7 +10,6 @@ const SignInForm = (props) => {
     
     const [signInEmailInput, setSignInEmailInput]       = useState("");
     const [signInPasswordInput, setSignInPasswordInput] = useState("");
-    const [rememberMeCheckbox, setRememberMeCheckbox]   = useState(null);
     const [getUserName, setGetUserName] = useState(false);
     //destructuring props
     const { loadUsersDataFromDatabase } = props;
@@ -18,8 +17,6 @@ const SignInForm = (props) => {
     const onEmailChange = (event) => {setSignInEmailInput(event.target.value)};
     //get password input
     const onPasswordChange = (event) => {setSignInPasswordInput(event.target.value)};
-    //
-    const onCheckboxChange = (event) => {setRememberMeCheckbox(event.target.value)};
     //Google OAuth2.0 button
     const googleLogin = () => {window.open('http://localhost:3050/auth/google', '_self')};
     //GitHub OAuth 2.0 button
@@ -35,27 +32,23 @@ const SignInForm = (props) => {
             method  : 'post',
             headers : {'Content-Type' : 'application/json'},
             body    : JSON.stringify({
-                email      : signInEmailInput.trim(),
-                password   : signInPasswordInput.trim()
+
+                    email      : signInEmailInput.trim(),
+                    password   : signInPasswordInput.trim()
+
             })
         })
         .then((response) => {
-            if(response.ok) {
-                return response.json();
-            }
-            else {
-                throw new Error ('Network response was not ok');
-            }
+            if(response.ok) { return response.json() }
+            else { throw new Error ('Network response was not ok') }
         })
         .then((data) => {
+            console.log(data)
              setGetUserName(data.user.name);
             
             if(data && data.user) {
                 loadUsersDataFromDatabase(data);
                  console.log('Success')
-                if (rememberMeCheckbox) {
-                    localStorage.setItem('user', JSON.stringify(data.user));
-                }
             };
         })
         .catch((error) => {
@@ -63,24 +56,6 @@ const SignInForm = (props) => {
             if(error.response) error.response.text().then((text)=> console.log("catch block: ",text))
         })
     }; 
-
-    // useEffect(() => {  // Without the useEffect hook, the code would try to set the value of rememberMeCheckBox 
-    //                    //->to true immediately after rendering the component, but since the document.cookie property is not available during server rendering and may not be immediately available after the component mounts, the value of rememberMeCheckBox may not be set correctly. 
-    //     const user = JSON.parse(localStorage.getItem('user')); //the user variable is being used to check if the user has already logged in before and if so, to automatically log them back in by setting the appropriate session data.
-    //     console.log("useEffect called");
-    //     console.log(user)
-    //     if (user) {
-    //         loadUsersDataFromDatabase({user}) 
-    //         .then(() => {
-    //             SignInPageConditionChangeFunction();
-    //           })
-    //           .catch((error) => {
-    //             console.error('error message: ',error);
-    //           },[loadUsersDataFromDatabase, SignInPageConditionChangeFunction]);
-    //     }
-    // }); // Using the useEffect hook with an empty dependency array ([]) ensures 
-                 //->that the code to set the state variable is only executed once after 
-                 //->the component mounts, which ensures that the value of rememberMeCheckBox is set correctly.
     return(
         <div>
             <main className="pa4 black-80">
@@ -132,7 +107,7 @@ const SignInForm = (props) => {
                         </div>
                             {/* Remember me checkbox */}
                         <label className="pa0 ma0 lh-copy f6 pointer">
-                            <input type="checkbox" name='remember' onChange={onCheckboxChange} />
+                            <input type="checkbox" name='remember' />
                             Remember me
                         </label>
 
