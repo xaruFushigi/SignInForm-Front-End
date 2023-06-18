41af2dadae0e1e18da65b608998680669a93bad4
xaruFushigi <xaru.fushigi@gmail.com>
2023-06-18 23:13:15 +0900

    Update App.js

--- signinform-front-end/src/App.js
+++ signinform-front-end/src/App.js
@@ -1,48 +1,71 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 //Routes related dependency
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //JS file components
-import {SignInForm, SignUpForm, NavigationBar, Home, UserAccount} from './components';
+import {
+  SignInForm,
+  SignUpForm,
+  NavigationBar,
+  Home,
+  UserAccount,
+} from "./components";
 //CSS file
-import './App.css';
-import 'tachyons';
-import './GlobalStyles.css';
-import { myContext } from './Context';
+import "./App.css";
+import "tachyons";
+import "./GlobalStyles.css";
+import { myContext } from "./Context";
 function App() {
-
+  const { isAuthenticated } = useContext(myContext);
   const [downloadUsersData, setDownloadUsersData] = useState({
-                                                              id: "",
-                                                              name: "",
-                                                              email: "",
-                                                              entries: 0,
-                                                              joined: ""
-                                                            }); 
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  });
   const userObject = useContext(myContext);
   //loads database from 'users' table
   const loadUsersDataFromDatabase = (usersDataFromDatabase) => {
     setDownloadUsersData({
-                          id      : usersDataFromDatabase.id,
-                          name    : usersDataFromDatabase.name,
-                          email   : usersDataFromDatabase.email,
-                          entries : usersDataFromDatabase.entries,
-                          joined  : usersDataFromDatabase.joined
-                        })
+      id: usersDataFromDatabase.id,
+      name: usersDataFromDatabase.name,
+      email: usersDataFromDatabase.email,
+      entries: usersDataFromDatabase.entries,
+      joined: usersDataFromDatabase.joined,
+    });
   };
 
   return (
     <div>
-                  <BrowserRouter>
-                    <NavigationBar />
-                    <Routes>
-                      <Route path='/' element={<Home />} />
-                      <Route path='/signup'
-                                exact element={ userObject && userObject.isSignedIn ? ( alert('signed in')) 
-                        : (<SignUpForm loadUsersDataFromDatabase={loadUsersDataFromDatabase}/>) }
-                      />
-                      <Route path='/signin' exact element={<SignInForm loadUsersDataFromDatabase={loadUsersDataFromDatabase}/>} />
-                      <Route path='/userAccount' element={<UserAccount />} />
-                    </Routes>
-                  </BrowserRouter>
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/signup"
+            exact
+            element={
+              userObject && isAuthenticated ? (
+                alert("signed in")
+              ) : (
+                <SignUpForm
+                  loadUsersDataFromDatabase={loadUsersDataFromDatabase}
+                />
+              )
+            }
+          />
+          <Route
+            path="/signin"
+            exact
+            element={
+              <SignInForm
+                loadUsersDataFromDatabase={loadUsersDataFromDatabase}
+              />
+            }
+          />
+          <Route path="/userAccount" element={<UserAccount />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
